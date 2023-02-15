878b14e8acdda3de0de60e2879e63419550f71f7
Patrick Dutra <leonardo@software-ltda.com.brpatrickdutra100@hotmail.com>
2023-02-14 22:45:23 -0300

    vetor

---
+++ modE aula15/vetor.js
@@ -0,0 +1,12 @@
+let valores = [2, 6, 3, 8];
+
+// console.log(valores)
+
+// console.log(valores[0]);
+// console.log(valores[1]);
+// console.log(valores[2]);
+// console.log(valores[3]);
+
+for (let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`);
+}
